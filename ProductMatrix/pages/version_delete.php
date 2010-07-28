1ee7459337b902593283471d04956f82a2c697d6
John Reese <jreese@leetcode.net>
2010-07-27 21:21:48 -0400

    Copyright update; it's really 2010 now

--- ProductMatrix/pages/version_delete.php
+++ ProductMatrix/pages/version_delete.php
@@ -1,5 +1,5 @@
 <?php
-# Copyright (C) 2008-2009	John Reese
+# Copyright (C) 2008-2010	John Reese
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
